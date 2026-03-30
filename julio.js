1f8ec1b4653481068cafff46cca01e8854f4a7cd
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-30 09:23:20 -0300

    enrico e matheus

---
+++ julio.js
@@ -0,0 +1,13 @@
+// let nome = prompt("Digite seu nome:");
+// alert(`Bem-vindo, ${nome}`);
+// console.log(`rodando no console!`);
+// console.log(`nome que o usuário inseriu: ${nome}`); 
+// let idade=parseInt(prompt((`digite sua idade:`)));
+// //let idadeInt=parseInt(idade);
+// //idade=parseInt(idade);
+// alert(`legal, você terá ${idade+25} daqui à 25 anos`);
+
+let num1=parseInt(prompt(`digite o primeiro numero:`));
+let num2=parseInt(prompt(`digite o segundo numero:`));
+console.log(`soma=${num1+num2}`);
+alert(`produto=${num1*num2}`);
